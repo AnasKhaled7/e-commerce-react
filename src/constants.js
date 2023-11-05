98696ebc2f32d46b45d777778a0d444af8a97e50
Anas Khaled <anas.elfayoumy7@gmail.com>
2023-11-05 16:57:28 +0200

    feat(structure): add redux structure

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const BASE_URL =
+  process.env.REACT_APP_ENV === "development" ? "http://localhost:5000" : "";
+
+export const API_URL = `${BASE_URL}/api/v1`;
+
+export const AUTH_URL = `${API_URL}/auth`;
+export const USERS_URL = `${API_URL}/users`;
+export const CATEGORIES_URL = `${API_URL}/categories`;
+export const BRANDS_URL = `${API_URL}/brands`;
+export const PRODUCTS_URL = `${API_URL}/products`;
+export const CARTS_URL = `${API_URL}/carts`;
+export const ORDERS_URL = `${API_URL}/orders`;
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import { apiSlice } from "./slices/apiSlice";
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: process.env.REACT_APP_ENV !== "production",
+});
+
+export default store;
