2d2d60bbdf9a5fdfe02b50daf5b4c72207683913
Anas Khaled <anas.elfayoumy7@gmail.com>
2023-11-13 00:20:48 +0200

    feat(hooks): add snackbar for displaying alerts

---
+++ src/hooks/useSnackbar.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Alert, Slide, Snackbar } from "@mui/material";
+
+export const useSnackbar = () => {
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState("");
+  const [severity, setSeverity] = useState("success");
+
+  const showSnackbar = (msg, color) => {
+    setSeverity(color);
+    setMessage(msg);
+    setOpen(true);
+  };
+
+  const hideSnackbar = (event, reason) => {
+    if (reason === "clickaway") return;
+    setOpen(false);
+  };
+
+  const TransitionUp = (props) => <Slide {...props} direction="up" />;
+
+  const SnackbarComponent = () => (
+    <Snackbar
+      open={open}
+      autoHideDuration={5000}
+      onClose={hideSnackbar}
+      TransitionComponent={TransitionUp}
+    >
+      <Alert
+        onClose={hideSnackbar}
+        variant="filled"
+        severity={severity}
+        sx={{ width: "100%" }}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+
+  return [showSnackbar, hideSnackbar, SnackbarComponent];
+};
