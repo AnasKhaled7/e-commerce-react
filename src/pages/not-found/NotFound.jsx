cf3b234727441e4f82101791165bb4e62991e9b1
Anas Khaled <anas.elfayoumy7@gmail.com>
2023-11-21 03:06:52 +0200

    feat(404): add 404 page

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,4 +1,38 @@
+import { Button, Container, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        py: 4,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 3,
+        minHeight: { xs: "calc(100vh - 56px)", sm: "calc(100vh - 64px)" },
+      }}
+    >
+      <Typography variant="h2" component="h2" fontWeight={700}>
+        404
+      </Typography>
+      <Typography variant="h5" component="h3" fontWeight={500}>
+        Page not found
+      </Typography>
+
+      <Typography textAlign="center">
+        Sorry, we couldn't find the page you're looking for.
+      </Typography>
+
+      <Button variant="outlined" size="large" onClick={() => navigate("")}>
+        Go back
+      </Button>
+    </Container>
+  );
 };
+
 export default NotFound;
